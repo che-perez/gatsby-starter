e61508badba62869fbc26b2c732fbe3efc4b9bbb
Josue Perez <che.perez3@gmail.com>
2019-08-22 22:35:38 -0400

    add scss to project

--- src/components/SingleBlog.jsx
+++ src/components/SingleBlog.jsx
@@ -1,6 +1,8 @@
 import React from "react"
 import { graphql } from "gatsby"
 
+import("../scss/styles.scss");
+
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
 }) {
